4e0550acb408abdc75df07c86098a93ad1e03e12
Lewis Broerman <l.broerman@gmail.com>
2021-09-21 17:59:04 -0700

    adding shaman epic dot

--- data/spellsByClass.py
+++ data/spellsByClass.py
@@ -4,6 +4,15 @@ from data.spellColors import *
 # all classes atm
 def getSpellsNecro():
     return {
+        #shaman
+        "Bane of Nife" : Spell("Bane of Nife", 7, 860, poison()),
+        "Pox of Bertoxxulous" : Spell("Pox of Bertoxxulous", 18, 579, disease()),
+        "Curse of the Spirits" : Spell("Curse of the Spirits", 14, 1, magic()),
+        #druid
+        "Winged Death" : Spell("Winged Death", 9, 468, magic()),
+        "Wrath of Nature" : Spell("Wrath of Nature", 30, 1, magic()),
+        "Drones of Doom" : Spell("Drones of Doom", 8, 287, magic()),
+        #necro
         #Poison
         "Bane of Nife" : Spell("Bane of Nife", 7, 860, poison()),
         "Envenomed Bolt" : Spell("Envenomed Bolt", 6, 409, poison()),
@@ -18,15 +27,10 @@ def getSpellsNecro():
         "Boil Blood" : Spell("Boil Blood", 7, 136, fire()),
         "Heat Blood" : Spell("Heat Blood", 6, 88, fire()),
         #disease
-        "Pox of Bertoxxulous" : Spell("Pox of Bertoxxulous", 18, 579, disease()),
         "Plague" : Spell("Plague", 13, 308, disease()),
         "Cessation of Cor" : Spell("Cessation of Cor", 9, 203, disease()),
         #snare
         "Devouring Darkness" : Spell("Devouring Darkness", 13, 400, snare()),
         "Cascading Darkness" : Spell("Cascading Darkness", 16, 300, snare()),
         "Torment of Shadows" : Spell("Torment of Shadows", 16, 1, snare()),
-        #druid
-        "Winged Death" : Spell("Winged Death", 9, 468, magic()),
-        "Wrath of Nature" : Spell("Wrath of Nature", 30, 1, magic()),
-        "Drones of Doom" : Spell("Drones of Doom", 8, 287, magic()),
     }
