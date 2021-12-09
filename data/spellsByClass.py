2ae653920a9d8f68a2693ce144fd4da0a23a5341
Lewis Broerman <l.broerman@gmail.com>
2021-12-08 18:20:13 -0800

    Adding PoP spells

--- data/spellsByClass.py
+++ data/spellsByClass.py
@@ -14,10 +14,12 @@ def getSpellsNecro():
         "Wrath of Nature" : Spell("Wrath of Nature", 30, 1, magic()),
         "Drones of Doom" : Spell("Drones of Doom", 8, 287, magic()),
         #necro
-        #Poison
+        #Poison Blood of Thule
+        "Blood of Thule" : Spell("Blood of Thule", 7, 436, poison()),
         "Bane of Nife" : Spell("Bane of Nife", 7, 860, poison()),
         "Envenomed Bolt" : Spell("Envenomed Bolt", 6, 409, poison()),
         #Magic
+        "Saryrn's Kiss" : Spell("Saryrn's Kiss", 10, 550, magic()),
         "Zevfeer's Theft of Vitae" : Spell("Zevfeer's Theft of Vitae", 9, 650, magic()),
         "Vexing Mordinia" : Spell("Vexing Mordinia", 9, 495, magic()),
         "Morternum" : Spell("Morternum", 9, 1, magic()),
@@ -30,9 +32,11 @@ def getSpellsNecro():
         "Boil Blood" : Spell("Boil Blood", 7, 136, fire()),
         "Heat Blood" : Spell("Heat Blood", 6, 88, fire()),
         #disease
+        "Dark Plague" : Spell("Dark Plague", 12, 308, disease()),
         "Plague" : Spell("Plague", 13, 308, disease()),
         "Cessation of Cor" : Spell("Cessation of Cor", 9, 203, disease()),
-        #snare
+        #snare 
+        "Embracing Darkness" : Spell("Embracing Darkness", 20, 200, snare()),
         "Devouring Darkness" : Spell("Devouring Darkness", 13, 400, snare()),
         "Cascading Darkness" : Spell("Cascading Darkness", 16, 300, snare()),
         "Torment of Shadows" : Spell("Torment of Shadows", 16, 1, snare()),
